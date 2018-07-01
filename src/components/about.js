6aa2c2c475c11abe40ed10e67ee125e30737764c
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-07-01 00:21:50 -0700

    test fisho

--- src/components/about.js
+++ src/components/about.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
 import '../css/titleSection.css';
 import profileImage from '../images/me.png';
-import fish from '../images/fish.png';
 
 
 class TitleSection extends Component {
-
-  componentDidMount(){
-    const elem = document.getElementById("fish");
-    elem.style.webkittransform = 'scaleX(-1)';
-    elem.style.transform = 'scaleX(-1)';
-    let pos = 0;
-    let limit = window.innerWidth;
-    elem.style.top = '50%';
-    elem.style.left = '100%';
-    let id = setInterval(frame, 10);
-
-    function frame() {
-      if (pos == limit) {
-        clearInterval(id);
-      }
-      else {
-        pos++;
-        elem.style.left = pos + 'px';
-      }
-    }
-  }
-
-
   render() {
     return (
       <div className="titleSection d-flex">
-        <img src={fish} id='fish' />
         <div className='centeredDiv justify-content-center align-self-center'>
-
-
 
           <div className='row'>
             <div className='col-12 d-flex justify-content-center align-self-center'>
@@ -51,6 +24,7 @@ class TitleSection extends Component {
 
           </div>
         </div>
+
       </div>
     );
   }
