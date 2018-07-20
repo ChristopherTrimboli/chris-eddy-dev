a7de75c5ea7c5475a2e0244b4cd43a993dfcd906
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-07-19 23:51:39 -0700

    update portfolio with docs and change icon colors

--- src/components/portfolioSection.js
+++ src/components/portfolioSection.js
@@ -17,22 +17,22 @@ class PortfolioSection extends Component {
     });
 
     const freshThreadsDiv = document.getElementById('freshThreadsCol');
-    const bulldogDiv = document.getElementById('bulldogCol');
+    const docsDiv = document.getElementById('docsCol');
     const exokitDiv = document.getElementById('exokitCol');
 
     if(event.target.value === 'freshthreads'){
       freshThreadsDiv.hidden = false;
-      bulldogDiv.hidden = true;
+      docsDiv.hidden = true;
       exokitDiv.hidden = true;
     }
-    if(event.target.value === 'bulldog'){
+    if(event.target.value === 'docs'){
       freshThreadsDiv.hidden = true;
-      bulldogDiv.hidden = false;
+      docsDiv.hidden = false;
       exokitDiv.hidden = true;
     }
     if(event.target.value === 'exokit'){
       freshThreadsDiv.hidden = true;
-      bulldogDiv.hidden = true;
+      docsDiv.hidden = true;
       exokitDiv.hidden = false;
     }
 
@@ -40,10 +40,10 @@ class PortfolioSection extends Component {
 
   componentDidMount(){
     const freshThreadsDiv = document.getElementById('freshThreadsCol');
-    const bulldogDiv = document.getElementById('bulldogCol');
+    const docsDiv = document.getElementById('docsCol');
     const exokitDiv = document.getElementById('exokitCol');
     freshThreadsDiv.hidden = false;
-    bulldogDiv.hidden = true;
+    docsDiv.hidden = true;
     exokitDiv.hidden = true;
   }
 
@@ -75,40 +75,40 @@ class PortfolioSection extends Component {
             <h4>Navigate</h4>
             <div className="btn-group pb-1" data-toggle="buttons">
               <button type="button" className="btn btn-success focus active" aria-pressed='true' value='freshthreads' onClick={ this.handleChange }>Fresh-Threads</button>
-              <button type="button" className="btn btn-secondary" value='bulldog' onClick={ this.handleChange }>Bulldog Auto</button>
+              <button type="button" className="btn btn-secondary" value='docs' onClick={ this.handleChange }>Exokit Docs</button>
               <button type="button" className="btn btn-primary" value='exokit' onClick={ this.handleChange }>Exokit</button>
             </div>
             <div className="jumbotron jumbotron1"/>
             <h3>Fresh-Threads</h3>
-            <p>A shopping-cart mock-up that I made... LAMP stack, PHP 7 and mySQL hosted on a AWS Lightsail server.</p>
+            <p className='text-left'>This site is a shopping-cart mock-up that I made in school. It is a LAMP stack, built with Bootstrap and hosted on a AWS Lightsail server.</p>
             <a className="btn btn-primary btn-lg mr-2" href="https://www.fresh-threads.ca/fresh-threads/php/shop/shop.php" role="button">Visit Website</a>
             <a className="btn btn-secondary btn-lg" href="https://github.com/ChrisEddy/fresh-threads" role="button">Github Repo</a>
           </div>
 
-          <div id='bulldogCol' className='col-lg-8 col-md-12 col-sm-12 col-xs-12'>
+          <div id='docsCol' className='col-lg-8 col-md-12 col-sm-12 col-xs-12'>
             <h4>Navigate</h4>
             <div className="btn-group pb-1" data-toggle="buttons">
               <button type="button" className="btn btn-success" value='freshthreads' aria-pressed='false' onClick={ this.handleChange }>Fresh-Threads</button>
-              <button type="button" className="btn btn-secondary focus active" aria-pressed='true' value='bulldog' onClick={ this.handleChange }>Bulldog Auto</button>
+              <button type="button" className="btn btn-secondary focus active" aria-pressed='true' value='docs' onClick={ this.handleChange }>Exokit Docs</button>
               <button type="button" className="btn btn-primary" value='exokit' aria-pressed='false' onClick={ this.handleChange }>Exokit</button>
             </div>
             <div className="jumbotron jumbotron2"/>
-            <h3>Bulldog Autoworks</h3>
-            <p>This was the first site I ever made, it is built with Bootstrap, Jquery, JS and features some interesting google reviews code.</p>
-            <a className="btn btn-primary btn-lg mr-2" href="https://bulldogautoworks.ca/" role="button">Visit Website</a>
-            <a className="btn btn-secondary btn-lg" href="https://github.com/ChrisEddy/bulldog-autowork" role="button">Github Repo</a>
+            <h3>Exokit Docs</h3>
+            <p className='text-left'>I am a main developer for the Exokit technical documentation. This stack is built with Docusaurus, React and features Algolia search functionality.</p>
+            <a className="btn btn-primary btn-lg mr-2" href="https://docs.webmr.io/" role="button">Visit Website</a>
+            <a className="btn btn-secondary btn-lg" href="https://github.com/webmixedreality/webmr-docs" role="button">Github Repo</a>
           </div>
 
           <div id='exokitCol' className='col-lg-8 col-md-12 col-sm-12 col-xs-12'>
             <h4>Navigate</h4>
             <div className="btn-group pb-1" data-toggle="buttons">
               <button type="button" className="btn btn-success" value='freshthreads' aria-pressed='false' onClick={ this.handleChange }>Fresh-Threads</button>
-              <button type="button" className="btn btn-secondary" value='bulldog' aria-pressed='false' onClick={ this.handleChange }>Bulldog Auto</button>
+              <button type="button" className="btn btn-secondary" value='docs' aria-pressed='false' onClick={ this.handleChange }>Exokit Docs</button>
               <button type="button" className="btn btn-primary focus active" value='exokit' aria-pressed='true' onClick={ this.handleChange }>Exokit</button>
             </div>
             <div className="jumbotron jumbotron3"/>
             <h3>Exokit</h3>
-            <p>This is a landing page that is ongoing for the Exokit VR/AR browser. I am leading the site design on this project.</p>
+            <p className='text-left'>This is a landing page that is ongoing for the Exokit VR/AR browser. I am leading the site design on this project. This site is built with React, Mailchimp and Redux.</p>
             <a className="btn btn-primary btn-lg mr-2" href="https://exokit.webmr.io/" role="button">Visit Website</a>
             <a className="btn btn-secondary btn-lg" href="https://github.com/ChrisEddy/exokit-site" role="button">Github Repo</a>
           </div>
